c518f948f9aa3204bb862fe9cb8e0182af13ecdd
dpham123 <36905285+dpham123@users.noreply.github.com>
2018-11-28 19:26:20 -0800

    Initial commit

---
+++ src/genefinderhmm/HMM.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genefinderhmm;
+
+/**
+ *
+ * @author Unknown
+ */
+public class HMM {
+    
+}
