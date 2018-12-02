f755dfafe3aef240d10a0ed13590807c641cdb40
dpham123 <36905285+dpham123@users.noreply.github.com>
2018-12-01 23:29:41 -0800

    Implement partially working viterbi algorithm

--- src/genefinderhmm/HMM.java
+++ src/genefinderhmm/HMM.java
@@ -9,8 +9,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,81 +18,126 @@ import java.util.logging.Logger;
  * @author dpham123
  */
 class HMM {
-    private Set<State> states;
+
+    private State[] states;
     private String sequence;
     private double[][] transitionProbabilities;
     private double[][] dynamicTable;
-    
-    HMM(File states, File transitions, File sequence) throws IOException{
+
+    HMM(File states, File transitions, File sequence) throws IOException {
         this.sequence = "";
         parseFile(states, transitions, sequence);
-        dynamicTable = new double[this.states.size()][this.sequence.length()];
+        dynamicTable = new double[this.states.length][this.sequence.length()];
     }
-    
-    private void parseFile(File states_fileName, File transitions_fileName, File sequence_fileName) throws IOException{
+
+    private void parseFile(File states_fileName, File transitions_fileName, File sequence_fileName) throws IOException {
         FileReader fr = new FileReader(states_fileName);
         BufferedReader br = new BufferedReader(fr);
-        
+
         String line = br.readLine();
-        states = new HashSet<>();
-        
+        LinkedList states = new LinkedList<>();
+
         // Parses states file
-        while (line != null){
+        while (line != null) {
             String[] splitStuff = line.split("	");
             states.add(new State(splitStuff));
             line = br.readLine();
         }
-        
+        this.states = (State[]) states.toArray(new State[0]);
+
         fr = new FileReader(transitions_fileName);
         br = new BufferedReader(fr);
         line = br.readLine();
         line = br.readLine();
         String[] tempSplit = line.split("	");
         transitionProbabilities = new double[tempSplit.length][tempSplit.length];
-        
+
         // Parses transitions file
         int counter = 1;
-        while (line != null){
+        while (line != null) {
             String[] splitStuff = line.split("	");
-            for (int i = 1; i < splitStuff.length; i++){
+            for (int i = 1; i < splitStuff.length; i++) {
                 transitionProbabilities[counter][i] = Double.parseDouble(splitStuff[i]);
             }
             line = br.readLine();
             counter++;
         }
-        
+
         fr = new FileReader(sequence_fileName);
         br = new BufferedReader(fr);
         line = br.readLine();
         line = br.readLine();
-        
+
         // Parses observations file
-        while (line != null){
+        while (line != null) {
             sequence += line;
             line = br.readLine();
         }
         
-        for (int i = 0; i < transitionProbabilities.length; i++){
-            for (int j = 0; j < transitionProbabilities[0].length; j++){
+        System.out.println("Transition Probabilities");
+        System.out.println("----------------------------------------------");
+        for (int i = 0; i < transitionProbabilities.length; i++) {
+            for (int j = 0; j < transitionProbabilities[0].length; j++) {
                 System.out.print(transitionProbabilities[i][j] + " ");
             }
             System.out.println();
         }
-        
+
     }
-    
-    void viterbi(){
-        
-        for (int i = 1; i < sequence.length(); i++){
-            
+
+    private void fillInTableEntry(int row, int column, double probability) {
+        dynamicTable[row][column] = probability;
+    }
+
+    private double max(double[] numbers) {
+        double max = numbers[0];
+
+        for (double d : numbers) {
+            if (d > max) {
+                max = d;
+            }
+        }
+        return max;
+    }
+
+    void viterbi() {
+        fillInTableEntry(0, 0, Math.log10(states[0].getEmissionProbability(sequence.substring(0, 1))));
+        fillInTableEntry(1, 0, Math.log10(0));
+        fillInTableEntry(2, 0, Math.log10(0));
+        fillInTableEntry(3, 0, Math.log10(0));
+        double[] singleStateMax = new double[states.length];
+        for (int i = 1; i < sequence.length(); i++) {
+            for (int j = 0; j < states.length; j++) {
+                for (int k = 0; k < states.length; k++) {
+                    singleStateMax[k] = dynamicTable[j][i - 1] + Math.log10(transitionProbabilities[k + 1][j + 1]);
+                    System.out.println(dynamicTable[j][i - 1]);
+
+                    if (!states[k].getName().equals(".Intergenic") && i + 2 < sequence.length()) {
+                        singleStateMax[k] += Math.log10(states[j].getEmissionProbability(sequence.substring(i, i + 3)));
+                    } else if (states[k].getName().equals(".Intergenic")){
+                        singleStateMax[k] += Math.log10(states[j].getEmissionProbability(sequence.substring(i, i + 1)));
+                    }
+                }
+                fillInTableEntry(j, i, max(singleStateMax));
+            }
+        }
+        System.out.println("Dynamic Table");
+        System.out.println("----------------------------------------------");
+        for (int i = 0; i < dynamicTable[0].length; i++) {
+            System.out.println(i);
+            System.out.println("-------------------");
+            for (int j = 0; j < dynamicTable.length; j++) {
+                System.out.println(dynamicTable[j][i] + " ");
+            }
         }
     }
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         try {
-            HMM test = new HMM(new File("data/Ecoli_states.txt"), 
-                    new File("data/Ecoli_transitions.txt"), new File("data/Ecoli_observation_290.txt"));
-            
+            HMM test = new HMM(new File("data/Ecoli_states.txt"),
+                    new File("data/Ecoli_transitions.txt"), new File("data/test_observation.txt"));
+            test.viterbi();
+
         } catch (IOException ex) {
             Logger.getLogger(HMM.class.getName()).log(Level.SEVERE, null, ex);
         }
